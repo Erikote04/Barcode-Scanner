98ffd4e98f34eda0850f14855a701804c85fba54
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-19 01:06:26 +0200

    View life cicle

--- BarcodeScanner/BarcodeScanner/ScannerViewController.swift
+++ BarcodeScanner/BarcodeScanner/ScannerViewController.swift
@@ -14,7 +14,7 @@ enum CameraError: String {
 }
 
 protocol ScannerViewControllerDelegate: AnyObject {
-    func didFind(barCode: String)
+    func didFind(barcode: String)
     func didSurface(error: CameraError)
 }
 
@@ -31,6 +31,22 @@ final class ScannerViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let previewLayer  = previewLayer else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
     }
     
     private func setupCaptureSession() {
@@ -92,6 +108,6 @@ extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         
-        scannerDelegate?.didFind(barCode: barcodeString)
+        scannerDelegate?.didFind(barcode: barcodeString)
     }
 }
