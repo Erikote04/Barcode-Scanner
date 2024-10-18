527f5b4e2a1d33ed49208d3fe80582d8fd4638b5
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-19 00:38:21 +0200

    Setting up the camera

---
+++ BarcodeScanner/BarcodeScanner/ScannerViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ScannerViewController.swift
+//  BarcodeScanner
+//
+//  Created by Erik Sebastian de Erice Jerez on 19/10/24.
+//
+
+import AVFoundation
+import UIKit
+
+protocol ScannerViewControllerDelegate: AnyObject {
+    func didFind(barCode: String)
+}
+
+final class ScannerViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerViewControllerDelegate?
+    
+    init(scannerDelegate: ScannerViewControllerDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: .main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
+        } else {
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+    }
+}
+
+extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let metadataObject = metadataObjects.first else { return }
+        guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+        guard let barcodeString = readableObject.stringValue else { return }
+        
+        scannerDelegate?.didFind(barCode: barcodeString)
+    }
+}
